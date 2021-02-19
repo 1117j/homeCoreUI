58d0f669662e3f66269cf93a2e7311368f6f51ae
1117j <leehjin17@gmail.com>
2021-02-19 18:03:36 +0900

    nav하려고 했는데 Home만 하고 실패 8ㅅ8

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+/* import { component } from 'vue/types/umd'
+ */
+const Home = () => import('@/views/Home')
+const About = () => import('@/views/About')
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+export default new VueRouter({
+  mode: 'history', // https://router.vuejs.org/api/#mode
+  linkActiveClass: 'active',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: configRoutes()
+})
 
-const router = new VueRouter({
+/* const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+ */
 
-export default router
+function configRoutes () {
+  return [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+}
